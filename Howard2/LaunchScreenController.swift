79bf5595bbdcacaf66d74e1afdba41eacf1abefb
Noah Mintz Roberts <nmintzro@uvm.edu>
2019-04-04 10:20:51 -0400

    Added necessary storyboards and controllers for every page of the app.
    Implemented login/signup

---
+++ Howard2/LaunchScreenController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenController.swift
+//  Howard2
+//
+//  Created by Noah Mintz Roberts on 4/4/19.
+//  Copyright © 2019 William Risigo. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
