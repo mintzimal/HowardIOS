571869bc18dcf767dc432bf002ac30d6bf62ad87
Billy Risigo <wlrisigo@gmail.com>
2019-04-24 19:45:35 -0400

    Fits all screens. Working on resizing

--- Howard2/AppsController.swift
+++ Howard2/AppsController.swift
@@ -16,15 +16,27 @@ class AppsController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Twitter(_ sender: Any) {
+        if let appURL = URL(string: "twitter//:"){
+            let canOpen = UIApplication.shared.canOpenURL(appURL)
+            print("\(canOpen)")
+            let appName = "Twitter"
+            let appScheme = "\(appName)://"
+            let appSchemeURL = URL(string: appScheme)
+            
+            
+            if UIApplication.shared.canOpenURL(appSchemeURL! as URL){
+                UIApplication.shared.open(appSchemeURL!, options: [:], completionHandler: nil)
+            }
+            else{
+                let alert=UIAlertController(title: "\(appName) Error...", message: "The app \(appName) was not found", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title:"OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+    
+  
+        }
+    
     }
-    */
 
 }
