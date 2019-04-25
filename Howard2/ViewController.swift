a02b28653e41fae822806bd0332504f7c3596fa3
Griffin <gdcohen@uvm.edu>
2019-04-25 10:26:00 -0400

    FlashLight Stuff

--- Howard2/ViewController.swift
+++ Howard2/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet weak var Help: UIButton!
     @IBOutlet weak var News: UIButton!
     @IBOutlet weak var Apps: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
-    
+    var isTorch = false
+    @IBAction func actionTorchClick(sender: AnyObject) {
+        
+        
+        
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        
+        if device.hasTorch {
+            
+            isTorch = !isTorch
+            do {
+                try device.lockForConfiguration()
+                if isTorch == true {
+                    device.torchMode = .on
+                    
+                } else {
+                    device.torchMode = .off
+                    
+                }
+                
+                
+                
+                device.unlockForConfiguration()
+                
+            } catch {
+                
+                print("Torch is not working.")
+                
+            }
+            
+        } else {
+            
+            print("Torch not compatible with device.")
+            
+        }
+        
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
