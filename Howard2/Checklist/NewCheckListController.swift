34d9db1d116f2400060773a15fb407868b6db95b
Griffin <gdcohen@uvm.edu>
2019-04-30 12:21:47 -0400

    checklist changes

--- Howard2/Checklist/NewCheckListController.swift
+++ Howard2/Checklist/NewCheckListController.swift
@@ -20,18 +20,22 @@ struct appStruct {
 class NewCheckListController: UITableViewController {
     
     var arrayOfApps = [
-        appStruct(id: 1, title: "Medication Manager", text: "Helpful daily reminders", image: "PillReminder"),
-        appStruct(id: 2, title: "Google Calender", text: "So everything is in one place",image: "Calender.png"),
-        appStruct(id: 3, title: "Learn Something New", text: "New Tricks for Everyone",image: "thinker.png"),
-        appStruct(id: 4, title: "Youtube", text: "Watch YouTube right from this app!",image: "youtube"),
-        appStruct(id: 5, title: "Xylophone", text: "Mess with a basic instrument",image: "xylophone.jpg"),
-        appStruct(id: 6, title: "SSTA Bus Schedule", text: "See when transportation services arrives",image: "bus.jpg"),
-        appStruct(id:7, title: "Higher Ground Live Music", text: "Check performance schedules",image: "concert.jpg"),
-        appStruct(id:8, title: "Messages", text: "Receive messages from your safety connection specialist",image: "messages.jpg"),
-        appStruct(id:9, title: "Motivational", text: "Get motivational messages everyday!",image: "motivation.jpg"),
-        appStruct(id:10, title: "Text to Speech", text: "Type what you want to vocalize",image: "speech.jpeg")
+        appStruct(id: 0, title: "Medication Manager", text: "Helpful daily reminders", image: "PillReminder"),
+        appStruct(id: 1, title: "Google Calender", text: "So everything is in one place",image: "Calender.png"),
+        appStruct(id: 2, title: "Learn Something New", text: "New Tricks for Everyone",image: "thinker.png"),
+        appStruct(id: 3, title: "Youtube", text: "Watch YouTube right from this app!",image: "youtube"),
+        appStruct(id: 4, title: "Xylophone", text: "Mess with a basic instrument",image: "xylophone.jpg"),
+        appStruct(id: 5, title: "SSTA Bus Schedule", text: "See when transportation services arrives",image: "bus.jpg"),
+        appStruct(id:6, title: "Higher Ground Live Music", text: "Check performance schedules",image: "concert.jpg"),
+        appStruct(id:7, title: "Messages", text: "Receive messages from your safety connection specialist",image: "messages.jpg"),
+        appStruct(id:8, title: "Motivational", text: "Get motivational messages everyday!",image: "motivation.jpg"),
+        appStruct(id:9, title: "Text to Speech", text: "Type what you want to vocalize",image: "speech.jpeg")
         
     ]
+    
+    @IBOutlet var tableOutlet: UITableView!
+    @IBOutlet weak var Home: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,8 +43,24 @@ class NewCheckListController: UITableViewController {
         self.tableView.allowsMultipleSelection = true;
 
     }
+    
+    
         // MARK: - Table view data source
+    
+    @IBAction func getRows(_ sender: Any) {
         
+        var values : [Int] = []
+        var selected_indexPaths = tableOutlet!.indexPathsForSelectedRows
+        
+        for value in selected_indexPaths!{
+            values.append(value.row)
+        }
+        
+        print(values)
+        
+    }
+    
+    
         override func numberOfSections(in tableView: UITableView) -> Int {
             return 1
         }
@@ -66,8 +86,11 @@ class NewCheckListController: UITableViewController {
             let bgColorView = UIView()
             bgColorView.backgroundColor = .green
             cell.selectedBackgroundView = bgColorView
+            //NSArray *selectedCells = [self.tableView indexPathsForSelectedRows]
+
             
             return cell
         }
+    
         
 }
