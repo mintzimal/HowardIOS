297b78e0767e9f3d24e165c1667e2730f55ace08
Noah Mintz Roberts <nmintzro@uvm.edu>
2019-04-30 17:17:39 -0400

    Added row keys to the user defaults Moved the app data to the apps controller rather than the checklist Encoded the app data to make it transferable via user defaults to the checklist to populate

--- Howard2/Apps/AppsController.swift
+++ Howard2/Apps/AppsController.swift
@@ -9,6 +9,15 @@
 import UIKit
 import SafariServices
 
+struct appStruct:Codable {
+    
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+    
+}
+
 class AppsController: UIViewController, SFSafariViewControllerDelegate {
     
     @IBOutlet weak var AppOne: UIButton!
@@ -24,6 +33,20 @@ class AppsController: UIViewController, SFSafariViewControllerDelegate {
     @IBOutlet weak var LabelFour: UILabel!
     @IBOutlet weak var LabelFive: UILabel!
     @IBOutlet weak var LabelSix: UILabel!
+    
+    var arrayOfApps = [
+        appStruct(id: 0, title: "Medication Manager", text: "Helpful daily reminders", image: "PillReminder"),
+        appStruct(id: 1, title: "Google Calender", text: "So everything is in one place",image: "Calender.png"),
+        appStruct(id: 2, title: "Learn Something New", text: "New Tricks for Everyone",image: "thinker.png"),
+        appStruct(id: 3, title: "Youtube", text: "Watch YouTube right from this app!",image: "youtube"),
+        appStruct(id: 4, title: "Xylophone", text: "Mess with a basic instrument",image: "xylophone.jpg"),
+        appStruct(id: 5, title: "SSTA Bus Schedule", text: "See when transportation services arrives",image: "bus.jpg"),
+        appStruct(id:6, title: "Higher Ground Live Music", text: "Check performance schedules",image: "concert.jpg"),
+        appStruct(id:7, title: "Messages", text: "Receive messages from your safety connection specialist",image: "messages.jpg"),
+        appStruct(id:8, title: "Motivational", text: "Get motivational messages everyday!",image: "motivation.jpg"),
+        appStruct(id:9, title: "Text to Speech", text: "Type what you want to vocalize",image: "speech.jpeg")
+        
+    ]
     
     private var funFacts:String = "https://www.mentalfloss.com/amazingfactgenerator"
     private var calender:String = "https://calendar.google.com/calendar/"
@@ -46,6 +69,11 @@ class AppsController: UIViewController, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let selectedApps:[Int] = UserDefaults.standard.array(forKey: "AppsSelection") as? [Int] ?? [1,2,3,4,5,6]
+        
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(arrayOfApps), forKey:"AppFiles")
+        
 
         // Do any additional setup after loading the view.
     }
--- Howard2/Checklist/NewCheckListController.swift
+++ Howard2/Checklist/NewCheckListController.swift
@@ -8,30 +8,9 @@
 
 import UIKit
 
-struct appStruct {
-    
-    var id : Int
-    var title : String
-    var text : String
-    var image : String
-    
-}
-
 class NewCheckListController: UITableViewController {
     
-    var arrayOfApps = [
-        appStruct(id: 0, title: "Medication Manager", text: "Helpful daily reminders", image: "PillReminder"),
-        appStruct(id: 1, title: "Google Calender", text: "So everything is in one place",image: "Calender.png"),
-        appStruct(id: 2, title: "Learn Something New", text: "New Tricks for Everyone",image: "thinker.png"),
-        appStruct(id: 3, title: "Youtube", text: "Watch YouTube right from this app!",image: "youtube"),
-        appStruct(id: 4, title: "Xylophone", text: "Mess with a basic instrument",image: "xylophone.jpg"),
-        appStruct(id: 5, title: "SSTA Bus Schedule", text: "See when transportation services arrives",image: "bus.jpg"),
-        appStruct(id:6, title: "Higher Ground Live Music", text: "Check performance schedules",image: "concert.jpg"),
-        appStruct(id:7, title: "Messages", text: "Receive messages from your safety connection specialist",image: "messages.jpg"),
-        appStruct(id:8, title: "Motivational", text: "Get motivational messages everyday!",image: "motivation.jpg"),
-        appStruct(id:9, title: "Text to Speech", text: "Type what you want to vocalize",image: "speech.jpeg")
-        
-    ]
+    var arrayOfApps : [appStruct] = []
     
     @IBOutlet var tableOutlet: UITableView!
     @IBOutlet weak var Home: UIButton!
@@ -41,6 +20,12 @@ class NewCheckListController: UITableViewController {
         super.viewDidLoad()
         
         self.tableView.allowsMultipleSelection = true;
+        
+        guard var data = UserDefaults.standard.value(forKey:"AppFiles") as? Data else { return }
+        
+        let arrayOfApps = try? PropertyListDecoder().decode(Array<appStruct>.self, from: data)
+
+        self.arrayOfApps = arrayOfApps!
 
     }
     
@@ -56,7 +41,7 @@ class NewCheckListController: UITableViewController {
             values.append(value.row)
         }
         
-        print(values)
+        UserDefaults.standard.set(values, forKey: "AppSelection")
         
     }
     
@@ -76,12 +61,12 @@ class NewCheckListController: UITableViewController {
         override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "AppOne", for: indexPath)
             let apps = arrayOfApps[indexPath.row]
-            cell.textLabel?.text = arrayOfApps[indexPath.row].title;
-            cell.detailTextLabel?.text = arrayOfApps[indexPath.row].text
+            cell.textLabel?.text = (arrayOfApps[indexPath.row] as appStruct).title;
+            cell.detailTextLabel?.text = (arrayOfApps[indexPath.row] as! appStruct).text
             //var imageName = UIImage(named: arrayOfApps[indexPath.row].image)
             //cell.imageView?.image = arrayOfApps[indexPath.row].imageName
             //cell.imageView?.image = imageName
-            cell.imageView?.image = UIImage(named: apps.image)
+            cell.imageView?.image = UIImage(named: ((apps as! appStruct).image))
             
             let bgColorView = UIView()
             bgColorView.backgroundColor = .green
